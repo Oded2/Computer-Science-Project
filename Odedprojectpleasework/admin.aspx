9d0317464c02d6c0662bd20f4dd3bf02d6cd1d36
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-05-27 16:29:30 +0300

    In progress adding an admin page

--- Odedprojectpleasework/admin.aspx
+++ Odedprojectpleasework/admin.aspx
@@ -2,4 +2,22 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="adminParent">
+      <div class="content">
+        <table class="" runat="server" id="tblUsers">
+          <tr>
+            <th>Id</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Telephone</th>
+            <th>Address</th>
+            <th>Date of Birth</th>
+            <th>GPA</th>
+            <th>Email Address</th>
+            <th>Recieving mailing?</th>
+            <th>Action</th>
+          </tr>
+        </table>
+      </div>
+    </div>
 </asp:Content>
--- Odedprojectpleasework/admin.aspx.cs
+++ Odedprojectpleasework/admin.aspx.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Odedprojectpleasework
@@ -11,7 +13,39 @@ namespace Odedprojectpleasework
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //var userID = Session["user_id"];
+            //if (userID == null)
+            //{
+            //    Response.Redirect("unreachable.aspx");
+             //   return;
 
+
+            //}
+            String sql = "select * from Users order by Id";
+            var dt = MyAdoHelper.ExecuteDataTable("Database1.mdf", sql);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+             
+                var tr = new HtmlTableRow();
+                TblUtils.addTd(tr, dr, "Id");
+                TblUtils.addTd(tr, dr, "fname");
+
+                TblUtils.addTd(tr, dr, "lName");
+                TblUtils.addTd(tr, dr, "tel");
+                TblUtils.addTd(tr, dr, "address");
+                TblUtils.addTd(tr, dr, "dob");
+                TblUtils.addTd(tr, dr, "gpa");
+                TblUtils.addTd(tr, dr, "email");
+                TblUtils.addTd(tr, dr, "spam");
+
+                var td = new HtmlTableCell();
+                td.InnerHtml = "<a href=\"user.aspx?id=" + dr["id"] + "\">Edit</a>";
+                tr.Controls.Add(td);
+                tblUsers.Rows.Add(tr);
+
+
+            }
         }
     }
 }
